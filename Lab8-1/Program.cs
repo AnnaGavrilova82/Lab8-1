aeb679bcbd0b78ce96c90966c30eed2283dc35ae
IliaGavrilov <IliaGavrilov@ILIAGAVRILOV-HP>
2021-10-31 17:00:41 +0300

    Добавьте файлы проекта.

---
+++ Lab8-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = "D:\\";
+
+            if (Directory.Exists(Path))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(Path);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(Path);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
